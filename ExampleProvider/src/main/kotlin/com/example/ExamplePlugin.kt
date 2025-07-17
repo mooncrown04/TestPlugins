51ee06290176acfe654693311d3512736ed528eb
mooncrown04 <mooncrown04@gmail.com>
2025-07-17 23:25:18 +0300

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class powerSinemaPlugin : Plugin() {
+class ExamplePlugin : Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the following list manually.
         registerMainAPI(powerSinema(context, null))
