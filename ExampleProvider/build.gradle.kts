ece3609e8ad8cae18946cbf48748287ad3733385
mooncrown04 <mooncrown04@gmail.com>
2025-07-17 23:39:16 +0300

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,38 +1,33 @@
+version = 3
+
 dependencies {
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-}
-
-// Use an integer for version numbers
-version = 1
-
-cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
-    tvTypes = listOf("Movie")
-
-    requiresResources = true
-    language = "en"
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
 
 android {
     buildFeatures {
         buildConfig = true
-        viewBinding = true
     }
+
+    defaultConfig {
+        val apiKey = project.findProperty("tmdbApiKey")?.toString() ?: ""
+        buildConfigField("String", "TMDB_SECRET_API", "\"$apiKey\"")
+    }
+}
+
+cloudstream {
+    authors     = listOf("GitLatte", "patr0nq", "keyiflerolsun")
+    language    = "tr"
+    description = "powerboard`un sinema arşivi"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Movie")
+    iconUrl = "https://raw.githubusercontent.com/GitLatte/Sinetech/master/img/powersinema/powersinema.png"
 }
