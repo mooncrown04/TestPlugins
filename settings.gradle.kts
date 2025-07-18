653cfcfef9c1e98d41af704df8551b324b6f4f1f
mooncrown04 <mooncrown04@gmail.com>
2025-07-18 22:42:10 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,4 +29,4 @@ fun File.eachDir(block: (File) -> Unit) {
 }
 
 // Yalnızca tek bir projeyi dahil etmek için, önceki satırları (ilk hariç) yorumlayın ve eklentinizi şöyle dahil edin:
-include(":dizi",":ExampleProvider")
+include(":dizi",":ExampleProvider",":Pornhub")
