467b133b68283b68de2f902c691fd7ffb5c36812
mooncrown04 <mooncrown04@gmail.com>
2025-07-19 02:07:28 +0300

    Update PornhubProvider.kt

--- Pornhub/src/main/kotlin/com/owencz1998/PornhubProvider.kt
+++ Pornhub/src/main/kotlin/com/owencz1998/PornhubProvider.kt
@@ -5,7 +5,6 @@ import org.jsoup.nodes.Element
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
-import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.mvvm.logError
 import com.lagradost.cloudstream3.network.WebViewResolver
 import com.lagradost.cloudstream3.utils.newExtractorLink
@@ -15,96 +14,66 @@ import org.json.JSONObject
 
 class PornHubProvider : MainAPI() {
     private val globalTvType = TvType.NSFW
-    override var mainUrl              = "https://www.pornhub.com"
-    override var name                 = "PornHub"
-    override val hasMainPage          = true
-    override var lang                 = "en"
-    override val hasQuickSearch       = false
-    override val hasDownloadSupport   = true
+    override var mainUrl             = "https://www.pornhub.com"
+    override var name                = "PornHub"
+    override val hasMainPage         = true
+    override var lang                = "en"
+    override val hasQuickSearch      = false
+    override val hasDownloadSupport  = true
     override val hasChromecastSupport = true
-    override val supportedTypes       = setOf(TvType.NSFW)
-    override val vpnStatus            = VPNStatus.MightBeNeeded
+    override val supportedTypes      = setOf(TvType.NSFW)
+    override val vpnStatus           = VPNStatus.MightBeNeeded
 
     override val mainPage = mainPageOf(
-        "${mainUrl}/video/search?search=giannadior&page="      to "giannadior",
-        "${mainUrl}/video/search?search=abelladanger&page="    to "abelladanger",
-        "${mainUrl}/video/search?search=kyliepage&page="       to "kyliepage",
-        "${mainUrl}/video/search?search=augustames&page="      to  "augustames",
-        "${mainUrl}/video/search?search=elizaibarra&page="     to "elizaibarra",
-        "${mainUrl}/video/search?search=rileyreid&page="       to "rileyreid",
-        "${mainUrl}/video/search?search=miakhalifa&page="      to "miakhalifa",
-        "${mainUrl}/video/search?search=emilywillis&page="     to "emilywillis", 
-        "${mainUrl}/video/search?search=kyliequinn&page="      to "kyliequinn",
-        "${mainUrl}/video/search?search=sashagrey&page="       to "sashagrey",
-        "${mainUrl}/video/search?search=blacked&page="         to "blacked",
-        "${mainUrl}/video/search?search=johnnysins&page="      to "johnnysins",
-        "${mainUrl}/video/search?search=jasonluv&page="        to "jasonluv",
-        "${mainUrl}/video/search?search=jaxslayher&page="      to "jaxslayher",	
-	"${mainUrl}/video/search?search=blacked&page="         to "blacked",
-        "${mainUrl}/video/search?search=blackedraw&page="      to "blackedraw",
-        "${mainUrl}/video/search?search=tushy&page="           to "tushy",
-        "${mainUrl}/video/search?search=vixen&page="           to "vixen",
-	"${mainUrl}/video/search?search=brazzers&page="        to "brazzers",
-        "${mainUrl}/video/search?search=teamskeet&page="       to "teamskeet",     
-	"${mainUrl}/video?o=mr&hd=1&page="           to "Recently Featured",
-        "${mainUrl}/video?o=tr&t=w&hd=1&page="       to "Top Rated",
-        "${mainUrl}/video?o=mv&t=w&hd=1&page="       to "Most Viewed",
-        "${mainUrl}/video?o=ht&t=w&hd=1&page="       to "Hottest",
+        "${mainUrl}/video?o=mr&hd=1&page="        to "Recently Featured",
+        "${mainUrl}/video?o=tr&t=w&hd=1&page="    to "Top Rated",
+        "${mainUrl}/video?o=mv&t=w&hd=1&page="    to "Most Viewed",
+        "${mainUrl}/video?o=ht&t=w&hd=1&page="    to "Hottest",
         "${mainUrl}/video?p=professional&hd=1&page=" to "Professional",
-        "${mainUrl}/video?o=lg&hd=1&page="           to "Longest",
-        "${mainUrl}/video?p=homemade&hd=1&page="     to "Homemade",
-        "${mainUrl}/video?o=cm&t=w&hd=1&page="       to "Newest",
-        "${mainUrl}/video?c=35&page="                to       "Anal",
-        "${mainUrl}/video?c=27&page="                to "Lesbian",
-        "${mainUrl}/video?c=98&page="                to "Arab",
-        "${mainUrl}/video?c=1&page="                 to "Asian",
-        "${mainUrl}/video?c=89&page="                to "Babysitter", 
-        "${mainUrl}/video?c=6&page="                 to "BBW",
-        "${mainUrl}/video?c=141&page="               to "Behind The Scenes",
-        "${mainUrl}/video?c=4&page="                 to "Big Ass",
-        "${mainUrl}/video?c=7&page="                 to "Big Dick",
-        "${mainUrl}/video?c=8&page="                 to "Big Tits",
-        "${mainUrl}/video?c=13&page="                to "Blowjob",
-                       
+        "${mainUrl}/video?o=lg&hd=1&page="        to "Longest",
+        "${mainUrl}/video?p=homemade&hd=1&page="  to "Homemade",
+        "${mainUrl}/video?o=cm&t=w&hd=1&page="    to "Newest",
+        "${mainUrl}/video?c=35&page="             to "Anal",
+        "${mainUrl}/video?c=27&page="             to "Lesbian",
+        "${mainUrl}/video?c=98&page="             to "Arab",
+        "${mainUrl}/video?c=1&page="              to "Asian",
+        "${mainUrl}/video?c=89&page="             to "Babysitter",
+        "${mainUrl}/video?c=6&page="              to "BBW",
+        "${mainUrl}/video?c=141&page="            to "Behind The Scenes",
+        "${mainUrl}/video?c=4&page="              to "Big Ass",
+        "${mainUrl}/video?c=7&page="              to "Big Dick",
+        "${mainUrl}/video?c=8&page="              to "Big Tits",
+        "${mainUrl}/video?c=13&page="             to "Blowjob",
     )
+
     private val cookies = mapOf(Pair("hasVisited", "1"), Pair("accessAgeDisclaimerPH", "1"))
 
-   // İkinci dosyadan alınan toSearchResult uzantı fonksiyonu, ilk dosyanın seçicilerine uyarlandı
+    // toSearchResult uzantı fonksiyonu
     private fun Element.toSearchResult(): SearchResponse? {
-        // İlk dosyanın ana sayfa ve arama yapısına uygun seçiciler
         val title = this.selectFirst("span.title a")?.text() ?: return null
         val link = fixUrlNull(this.selectFirst("a")?.attr("href")) ?: return null
-        val posterUrl = fetchImgUrl(this.selectFirst("img")) // İlk dosyanın fetchImgUrl'si kullanılıyor
+        val posterUrl = fetchImgUrl(this.selectFirst("img"))
 
+        // MovieSearchResponse constructor'ına posterUrl'ı doğrudan parametre olarak geçiyoruz
+        // 'data' parametresi ve lambda kaldırıldı çünkü constructor'da beklenmiyor
         return MovieSearchResponse(
             name = title,
             url = link,
-            apiName = this@PornHubProvider.name, // Sınıfın 'name' özelliğine erişim
+            apiName = this@PornHubProvider.name,
             type = globalTvType,
-            posterUrl = posterUrl
+            posterUrl = posterUrl // posterUrl'ı doğrudan parametre olarak geçiyoruz
         )
     }
+
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         try {
             val categoryData = request.data
             val categoryName = request.name
             val pagedLink = if (page > 0) categoryData + page else categoryData
             val soup = app.get(pagedLink, cookies = cookies).document
-            val home = soup.select("div.sectionWrapper div.wrap").mapNotNull {
-                if (it == null) {
-                    return@mapNotNull null
-                }
-                val title = it.selectFirst("span.title a")?.text() ?: ""
-                val link = fixUrlNull(it.selectFirst("a")?.attr("href")) ?: return@mapNotNull null
-                val img = fetchImgUrl(it.selectFirst("img"))
-                MovieSearchResponse(
-                    name = title,
-                    url = link,
-                    apiName = this.name,
-                    type = globalTvType,
-                    posterUrl = img
-                )
-            }
+
+            val home = soup.select("div.sectionWrapper div.wrap").mapNotNull { it.toSearchResult() }
+
             if (home.isNotEmpty()) {
                 return newHomePageResponse(
                     list = HomePageList(
@@ -116,80 +85,63 @@ class PornHubProvider : MainAPI() {
             }
         } catch (e: Exception) {
             logError(e)
+            throw ErrorLoadingException()
         }
-        throw ErrorLoadingException()
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
         val url = "$mainUrl/video/search?search=${query}"
         val document = app.get(url, cookies = cookies).document
-        return document.select("div.sectionWrapper div.wrap").mapNotNull {
-            if (it == null) {
-                return@mapNotNull null
-            }
-            val title = it.selectFirst("span.title a")?.text() ?: return@mapNotNull null
-            val link = fixUrlNull(it.selectFirst("a")?.attr("href")) ?: return@mapNotNull null
-            val image = fetchImgUrl(it.selectFirst("img"))
-            MovieSearchResponse(
-                name = title,
-                url = link,
-                apiName = this.name,
-                type = globalTvType,
-                posterUrl = image
-            )
-        }.distinctBy { it.url }
+
+        return document.select("div.sectionWrapper div.wrap").mapNotNull { it.toSearchResult() }.distinctBy { it.url }
     }
 
-    override suspend fun load(url: String): LoadResponse {
-        val soup = app.get(url, cookies = cookies).document
-        val title = soup.selectFirst(".title span")?.text() ?: ""
-        val poster: String? = soup.selectFirst("div.video-wrapper .mainPlayerDiv img")?.attr("src")
-            ?: soup.selectFirst("head meta[property=og:image]")?.attr("content")
-         val year = Regex("""uploadDate": "(\d+)""").find(soup.html())?.groupValues?.get(1)?.toIntOrNull()
-        val rating = soup.selectFirst("span.percent")?.text()?.first()?.toString()?.toRatingInt()
-        val duration = Regex("duration' : '(.*)',").find(soup.html())?.groupValues?.get(1)?.toIntOrNull()
+    override suspend fun quickSearch(query: String): List<SearchResponse> = search(query)
 
-		
-		val tags = soup.select("div.categoriesWrapper a")
+    override suspend fun load(url: String): LoadResponse {
+        val document = app.get(url, cookies = cookies).document
+
+        val title = document.selectFirst("h1.title span[class='inlineFree']")?.text()?.trim() ?: ""
+        val poster: String? = document.selectFirst("div.video-wrapper .mainPlayerDiv img")?.attr("src")
+            ?: document.selectFirst("head meta[property=og:image]")?.attr("content")
+
+        val year = Regex("""uploadDate": "(\d+)""").find(document.html())?.groupValues?.get(1)?.toIntOrNull()
+        val rating = document.selectFirst("span.percent")?.text()?.first()?.toString()?.toRatingInt()
+        val duration = Regex("duration' : '(.*)',").find(document.html())?.groupValues?.get(1)?.toIntOrNull()
+
+        val tags = document.select("div.categoriesWrapper a[data-label='Category']")
             .map { it?.text()?.trim().toString().replace(", ", "") }
 
-        val recommendations = soup.select("ul#recommendedVideos li.pcVideoListItem").map {
-            val rTitle = it.selectFirst("div.phimage a")?.attr("title") ?: ""
-            val rUrl = fixUrl(it.selectFirst("div.phimage a")?.attr("href").toString())
-            val rPoster = fixUrl(
-                it.selectFirst("div.phimage img.js-videoThumb")?.attr("src").toString()
-            )
-            MovieSearchResponse(
-                name = rTitle, apiName = this.name, url = rUrl, posterUrl = rPoster
+        val recommendations = document.selectXpath("//a[contains(@class, 'img')]").mapNotNull {
+            val recName = it.attr("title").trim()
+            val recHref = fixUrlNull(it.attr("href")) ?: return@mapNotNull null
+            val recPosterUrl = fixUrlNull(it.selectFirst("img")?.attr("src"))
+            // newMovieSearchResponse'a posterUrl'ı doğrudan parametre olarak geçiyoruz
+            // ve 'data' parametresi kaldırıldı çünkü constructor'da beklenmiyor
+            newMovieSearchResponse(
+                name = recName,
+                url = recHref,
+                type = globalTvType,
+                posterUrl = recPosterUrl // posterUrl'ı doğrudan parametre olarak geçiyoruz
             )
         }
 
         val actors =
-            soup.select("div.video-wrapper div.video-info-row.userRow div.userInfo div.usernameWrap a")
+            document.select("div.pornstarsWrapper a[data-label='Pornstar']")
                 .mapNotNull {
-                    // İkinci dosyadan Actor nesnesi oluşturma mantığı
-                    Actor(it.text().trim(), it.select("img").attr("src")) // İkinci dosyada img.attr("src") vardı, bu da aktör posteri olabilir
+                    // Actor nesnesi oluşturulurken fetchImgUrl kullanılarak resim URL'si çekiliyor
+                    Actor(it.text().trim(), fetchImgUrl(it.selectFirst("img")))
                 }
-        val relatedVideo = soup.select("ul#relatedVideosCenter li.pcVideoListItem").map {
-            val rTitle = it.selectFirst("div.phimage a")?.attr("title") ?: ""
-            val rUrl = fixUrl(it.selectFirst("div.phimage a")?.attr("href").toString())
-            val rPoster = fixUrl(
-                it.selectFirst("div.phimage img.js-videoThumb")?.attr("src").toString()
-            )
-            MovieSearchResponse(
-                name = rTitle, apiName = this.name, url = rUrl, posterUrl = rPoster
-            )
-        }
 
         return newMovieLoadResponse(title, url, TvType.NSFW, url) {
             this.posterUrl = poster
-            this.year = year // Yeni eklenen
-            this.plot = title // Orijinalden korunuyor
+            this.year = year
+            this.plot = title
             this.tags = tags
-            this.rating = rating // Yeni eklenen
-            this.duration = duration // Yeni eklenen
+            this.rating = rating
+            this.duration = duration
+            this.recommendations = recommendations
             addActors(actors)
-            this.recommendations = recommendations + relatedVideo
         }
     }
 
@@ -223,10 +175,10 @@ class PornHubProvider : MainAPI() {
                             source = name,
                             name = "${this.name}",
                             url = stream.streamUrl,
-                            type = ExtractorLinkType.M3U8,
+                            type = ExtractorLinkType.M3u8,
                         ) {
                             this.quality = Regex("(\\d+)").find(quality ?: "")?.groupValues?.get(1)
-                            .let { getQualityFromName(it) }
+                                .let { getQualityFromName(it) }
                             this.referer = mainUrl
                         }
                     )
