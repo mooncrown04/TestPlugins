d48ae8a036c020f9df8f7b8c5a91bcc3ca0c7961
mooncrown04 <mooncrown04@gmail.com>
2025-07-18 23:49:39 +0300

    Update PornhubProvider.kt

--- Pornhub/src/main/kotlin/com/owencz1998/PornhubProvider.kt
+++ Pornhub/src/main/kotlin/com/owencz1998/PornhubProvider.kt
@@ -1,201 +1,66 @@
-package com.owencz1998
-
-import android.util.Log
-import org.jsoup.nodes.Element
-import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.*
-import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
-import com.lagradost.cloudstream3.mvvm.logError
-import com.lagradost.cloudstream3.network.WebViewResolver
-import com.lagradost.cloudstream3.utils.newExtractorLink
-import com.lagradost.cloudstream3.utils.M3u8Helper
-import com.lagradost.cloudstream3.utils.getQualityFromName
-import org.json.JSONObject
-
-class PornHubProvider : MainAPI() {
-    private val globalTvType = TvType.NSFW
-    override var mainUrl             = "https://www.pornhub.com"
-    override var name                = "PornHub"
-    override val hasMainPage         = true
-    override var lang                = "en"
-    override val hasQuickSearch      = false
-    override val hasDownloadSupport  = true
-    override val hasChromecastSupport = true
-    override val supportedTypes      = setOf(TvType.NSFW)
-    override val vpnStatus           = VPNStatus.MightBeNeeded
-
-    override val mainPage = mainPageOf(
-        "${mainUrl}/video?o=mr&hd=1&page="        to "Recently Featured",
-        "${mainUrl}/video?o=tr&t=w&hd=1&page="    to "Top Rated",
-        "${mainUrl}/video?o=mv&t=w&hd=1&page="    to "Most Viewed",
-        "${mainUrl}/video?o=ht&t=w&hd=1&page="    to "Hottest",
-        "${mainUrl}/video?p=professional&hd=1&page=" to "Professional",
-        "${mainUrl}/video?o=lg&hd=1&page="        to "Longest",
-        "${mainUrl}/video?p=homemade&hd=1&page="  to "Homemade",
-        "${mainUrl}/video?o=cm&t=w&hd=1&page="    to "Newest",
-        "${mainUrl}/video?c=35&page="             to "Anal",
-        "${mainUrl}/video?c=27&page="             to "Lesbian",
-        "${mainUrl}/video?c=98&page="             to "Arab",
-        "${mainUrl}/video?c=1&page="              to "Asian",
-        "${mainUrl}/video?c=89&page="             to "Babysitter",
-        "${mainUrl}/video?c=6&page="              to "BBW",
-        "${mainUrl}/video?c=141&page="            to "Behind The Scenes",
-        "${mainUrl}/video?c=4&page="              to "Big Ass",
-        "${mainUrl}/video?c=7&page="              to "Big Dick",
-        "${mainUrl}/video?c=8&page="              to "Big Tits",
-        "${mainUrl}/video?c=13&page="             to "Blowjob",
-    )
-
-    private val cookies = mapOf(Pair("hasVisited", "1"), Pair("accessAgeDisclaimerPH", "1"))
-
-    // toSearchResult uzantı fonksiyonu
-    private fun Element.toSearchResult(): SearchResponse? {
-        val title = this.selectFirst("span.title a")?.text() ?: return null
-        val link = fixUrlNull(this.selectFirst("a")?.attr("href")) ?: return null
-        val posterUrl = fetchImgUrl(this.selectFirst("img"))
-
-        // MovieSearchResponse constructor'ına posterUrl'ı ve data'yı doğrudan parametre olarak geçiyoruz
-        return MovieSearchResponse(
-            name = title,
-            url = link,
-            apiName = this@PornHubProvider.name,
-            type = globalTvType,
-            posterUrl = posterUrl, // posterUrl'ı doğrudan parametre olarak geçiyoruz
-            data = null // Map<String, String>? beklenen parametreye null geçiyoruz
-        )
-    }
-
-    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        try {
-            val categoryData = request.data
-            val categoryName = request.name
-            val pagedLink = if (page > 0) categoryData + page else categoryData
-            val soup = app.get(pagedLink, cookies = cookies).document
-
-            val home = soup.select("div.sectionWrapper div.wrap").mapNotNull { it.toSearchResult() }
-
-            if (home.isNotEmpty()) {
-                return newHomePageResponse(
-                    list = HomePageList(
-                        name = categoryName, list = home, isHorizontalImages = true
-                    ), hasNext = true
-                )
-            } else {
-                throw ErrorLoadingException("No homepage data found!")
-            }
-        } catch (e: Exception) {
-            logError(e)
-            throw ErrorLoadingException()
-        }
-    }
-
-    override suspend fun search(query: String): List<SearchResponse> {
-        val url = "$mainUrl/video/search?search=${query}"
-        val document = app.get(url, cookies = cookies).document
-
-        return document.select("div.sectionWrapper div.wrap").mapNotNull { it.toSearchResult() }.distinctBy { it.url }
-    }
-
-    override suspend fun quickSearch(query: String): List<SearchResponse> = search(query)
-
-    override suspend fun load(url: String): LoadResponse {
-        val document = app.get(url, cookies = cookies).document
-
-        val title = document.selectFirst("h1.title span[class='inlineFree']")?.text()?.trim() ?: ""
-        val poster: String? = document.selectFirst("div.video-wrapper .mainPlayerDiv img")?.attr("src")
-            ?: document.selectFirst("head meta[property=og:image]")?.attr("content")
-
-        val year = Regex("""uploadDate": "(\d+)""").find(document.html())?.groupValues?.get(1)?.toIntOrNull()
-        val rating = document.selectFirst("span.percent")?.text()?.first()?.toString()?.toRatingInt()
-        val duration = Regex("duration' : '(.*)',").find(document.html())?.groupValues?.get(1)?.toIntOrNull()
-
-        val tags = document.select("div.categoriesWrapper a[data-label='Category']")
-            .map { it?.text()?.trim().toString().replace(", ", "") }
-
-        val recommendations = document.selectXpath("//a[contains(@class, 'img')]").mapNotNull {
-            val recName = it.attr("title").trim()
-            val recHref = fixUrlNull(it.attr("href")) ?: return@mapNotNull null
-            val recPosterUrl = fixUrlNull(it.selectFirst("img")?.attr("src"))
-            // newMovieSearchResponse'a posterUrl'ı ve data'yı doğrudan parametre olarak geçiyoruz
-            newMovieSearchResponse(
-                name = recName,
-                url = recHref,
-                type = globalTvType,
-                posterUrl = recPosterUrl, // posterUrl'ı doğrudan parametre olarak geçiyoruz
-                data = null // Map<String, String>? beklenen parametreye null geçiyoruz
-            )
-        }
-
-        val actors =
-            document.select("div.pornstarsWrapper a[data-label='Pornstar']")
-                .mapNotNull {
-                    Actor(it.text().trim(), it.selectFirst("img")?.attr("src"))
-                }
-
-        return newMovieLoadResponse(title, url, TvType.NSFW, url) {
-            this.posterUrl = poster
-            this.year = year
-            this.plot = title
-            this.tags = tags
-            this.rating = rating
-            this.duration = duration
-            this.recommendations = recommendations
-            addActors(actors)
-        }
-    }
-
-    override suspend fun loadLinks(
-        data: String,
-        isCasting: Boolean,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ): Boolean {
-        val request = app.get(
-            url = data, cookies = cookies
-        )
-        val document = request.document
-        val mediaDefinitions = JSONObject(
-            document.selectXpath("//script[contains(text(),'flashvars')]").first()?.data()
-                ?.substringAfter("=")?.substringBefore(";")
-        ).getJSONArray("mediaDefinitions")
-
-        for (i in 0 until mediaDefinitions.length()) {
-            if (mediaDefinitions.getJSONObject(i).optString("quality") != null) {
-                val quality = mediaDefinitions.getJSONObject(i).getString("quality")
-                val videoUrl = mediaDefinitions.getJSONObject(i).getString("videoUrl")
-                val extlinkList = mutableListOf<ExtractorLink>()
-                M3u8Helper().m3u8Generation(
-                    M3u8Helper.M3u8Stream(
-                        videoUrl
-                    ), true
-                ).apmap { stream ->
-                    extlinkList.add(
-                        newExtractorLink(
-                            source = name,
-                            name = "${this.name}",
-                            url = stream.streamUrl,
-                            type = ExtractorLinkType.M3U8,
-                        ) {
-                            this.quality = Regex("(\\d+)").find(quality ?: "")?.groupValues?.get(1)
-                                .let { getQualityFromName(it) }
-                            this.referer = mainUrl
-                        }
-                    )
-                }
-                extlinkList.forEach(callback)
-            }
-        }
-
-        return true
-    }
-
-    private fun fetchImgUrl(imgsrc: Element?): String? {
-        return try {
-            imgsrc?.attr("src") ?: imgsrc?.attr("data-src") ?: imgsrc?.attr("data-mediabook")
-            ?: imgsrc?.attr("alt") ?: imgsrc?.attr("data-mediumthumb")
-            ?: imgsrc?.attr("data-thumb_url")
-        } catch (e: Exception) {
-            null
-        }
-    }
-}
+2025-07-18T20:46:25.1094213Z > Task :Pornhub:checkKotlinGradlePluginConfigurationErrors SKIPPED
+2025-07-18T20:46:25.1100418Z > Task :Pornhub:preBuild UP-TO-DATE
+2025-07-18T20:46:25.1107134Z > Task :Pornhub:preDebugBuild UP-TO-DATE
+2025-07-18T20:46:25.1112839Z > Task :Pornhub:generateDebugResValues
+2025-07-18T20:46:25.1118925Z > Task :Pornhub:generateDebugResources
+2025-07-18T20:46:25.1140324Z > Task :Pornhub:packageDebugResources
+2025-07-18T20:46:25.1143332Z > Task :ExampleProvider:parseDebugLocalResources
+2025-07-18T20:46:25.1149502Z > Task :dizi:generateDebugRFile
+2025-07-18T20:46:25.1150089Z > Task :Pornhub:parseDebugLocalResources
+2025-07-18T20:46:25.6094129Z > Task :ExampleProvider:generateDebugRFile
+2025-07-18T20:46:25.6095105Z > Task :Pornhub:generateDebugRFile
+2025-07-18T20:46:31.5117738Z e: file:///home/runner/work/TestPlugins/TestPlugins/src/Pornhub/src/main/kotlin/com/owencz1998/PornhubProvider.kt:64:13 No parameter with name 'data' found.
+2025-07-18T20:46:31.5120900Z e: file:///home/runner/work/TestPlugins/TestPlugins/src/Pornhub/src/main/kotlin/com/owencz1998/PornhubProvider.kt:124:17 No parameter with name 'posterUrl' found.
+2025-07-18T20:46:31.5124343Z e: file:///home/runner/work/TestPlugins/TestPlugins/src/Pornhub/src/main/kotlin/com/owencz1998/PornhubProvider.kt:125:17 No parameter with name 'data' found.
+2025-07-18T20:46:31.5127029Z 
+2025-07-18T20:46:31.5127789Z > Task :Pornhub:compileDebugKotlin FAILED
+2025-07-18T20:46:36.4095056Z 
+2025-07-18T20:46:36.4095621Z > Task :dizi:compileDebugKotlin
+2025-07-18T20:46:36.4098627Z w: file:///home/runner/work/TestPlugins/TestPlugins/src/dizi/src/main/kotlin/com/example/powerDizi.kt:400:17 'constructor(data: String, name: String? = ..., season: Int? = ..., episode: Int? = ..., posterUrl: String? = ..., rating: Int? = ..., description: String? = ..., date: Long? = ...): Episode' is deprecated. Use newEpisode with runTime included.
+2025-07-18T20:46:36.4100541Z 
+2025-07-18T20:46:36.4101123Z > Task :ExampleProvider:compileDebugKotlin
+2025-07-18T20:46:36.5090947Z 
+2025-07-18T20:46:36.5096421Z FAILURE: Build failed with an exception.
+2025-07-18T20:46:36.5100157Z 
+2025-07-18T20:46:36.5109344Z * What went wrong:
+2025-07-18T20:46:36.5110258Z Execution failed for task ':Pornhub:compileDebugKotlin'.
+2025-07-18T20:46:36.5112609Z > A failure occurred while executing org.jetbrains.kotlin.compilerRunner.GradleCompilerRunnerWithWorkers$GradleKotlinCompilerWorkAction
+2025-07-18T20:46:36.5114226Z    > Compilation error. See log for more details
+2025-07-18T20:46:36.5115459Z 
+2025-07-18T20:46:36.5115879Z * Try:
+2025-07-18T20:46:36.5117189Z > Run with --stacktrace option to get the stack trace.
+2025-07-18T20:46:36.5118271Z > Run with --info or --debug option to get more log output.
+2025-07-18T20:46:36.5119108Z > Run with --scan to get full insights.
+2025-07-18T20:46:36.5120790Z > Get more help at https://help.gradle.org.
+2025-07-18T20:46:36.5121147Z 
+2025-07-18T20:46:36.5121345Z BUILD FAILED in 26s
+2025-07-18T20:46:36.5125230Z 20 actionable tasks: 20 executed
+2025-07-18T20:46:36.9014985Z ##[error]Process completed with exit code 1.
+2025-07-18T20:46:36.9092242Z Post job cleanup.
+2025-07-18T20:46:37.0816434Z Post job cleanup.
+2025-07-18T20:46:37.1756283Z [command]/usr/bin/git version
+2025-07-18T20:46:37.1792617Z git version 2.50.1
+2025-07-18T20:46:37.1835494Z Temporarily overriding HOME='/home/runner/work/_temp/610fa1cc-9651-4c7a-bfce-5211d6f0a2fe' before making global git config changes
+2025-07-18T20:46:37.1836476Z Adding repository directory to the temporary git global config as a safe directory
+2025-07-18T20:46:37.1841029Z [command]/usr/bin/git config --global --add safe.directory /home/runner/work/TestPlugins/TestPlugins/builds
+2025-07-18T20:46:37.1877497Z [command]/usr/bin/git config --local --name-only --get-regexp core\.sshCommand
+2025-07-18T20:46:37.1909698Z [command]/usr/bin/git submodule foreach --recursive sh -c "git config --local --name-only --get-regexp 'core\.sshCommand' && git config --local --unset-all 'core.sshCommand' || :"
+2025-07-18T20:46:37.2131933Z [command]/usr/bin/git config --local --name-only --get-regexp http\.https\:\/\/github\.com\/\.extraheader
+2025-07-18T20:46:37.2153460Z http.https://github.com/.extraheader
+2025-07-18T20:46:37.2166390Z [command]/usr/bin/git config --local --unset-all http.https://github.com/.extraheader
+2025-07-18T20:46:37.2197995Z [command]/usr/bin/git submodule foreach --recursive sh -c "git config --local --name-only --get-regexp 'http\.https\:\/\/github\.com\/\.extraheader' && git config --local --unset-all 'http.https://github.com/.extraheader' || :"
+2025-07-18T20:46:37.2529126Z Post job cleanup.
+2025-07-18T20:46:37.3587868Z [command]/usr/bin/git version
+2025-07-18T20:46:37.3629215Z git version 2.50.1
+2025-07-18T20:46:37.3674684Z Temporarily overriding HOME='/home/runner/work/_temp/3999d89f-986a-4354-ac7d-cf02df45a12f' before making global git config changes
+2025-07-18T20:46:37.3676535Z Adding repository directory to the temporary git global config as a safe directory
+2025-07-18T20:46:37.3689707Z [command]/usr/bin/git config --global --add safe.directory /home/runner/work/TestPlugins/TestPlugins/src
+2025-07-18T20:46:37.3725251Z [command]/usr/bin/git config --local --name-only --get-regexp core\.sshCommand
+2025-07-18T20:46:37.3760133Z [command]/usr/bin/git submodule foreach --recursive sh -c "git config --local --name-only --get-regexp 'core\.sshCommand' && git config --local --unset-all 'core.sshCommand' || :"
+2025-07-18T20:46:37.4007671Z [command]/usr/bin/git config --local --name-only --get-regexp http\.https\:\/\/github\.com\/\.extraheader
+2025-07-18T20:46:37.4030888Z http.https://github.com/.extraheader
+2025-07-18T20:46:37.4044104Z [command]/usr/bin/git config --local --unset-all http.https://github.com/.extraheader
+2025-07-18T20:46:37.4078619Z [command]/usr/bin/git submodule foreach --recursive sh -c "git config --local --name-only --get-regexp 'http\.https\:\/\/github\.com\/\.extraheader' && git config --local --unset-all 'http.https://github.com/.extraheader' || :"
+2025-07-18T20:46:37.4418186Z Cleaning up orphan processes
+2025-07-18T20:46:37.4691933Z Terminate orphan process: pid (2476) (java)
+2025-07-18T20:46:37.4721720Z Terminate orphan process: pid (2563) (java)
