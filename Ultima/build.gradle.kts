4bf9353309c8ef23666718b2ee076e144d511817
mooncrown04 <mooncrown04@gmail.com>
2025-07-19 17:16:39 +0300

    Update build.gradle.kts

--- Ultima/build.gradle.kts
+++ Ultima/build.gradle.kts
@@ -1,33 +1,44 @@
-version = 3
+@file:Suppress("UnstableApiUsage")
+
+import org.jetbrains.kotlin.konan.properties.Properties
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.google.android.material:material:1.12.0")
+}
+// use an integer for version numbers
+version = 41
+
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = "The ultimate All-in-One home screen to access all of your extensions at one place (You need to select/deselect sections in Ultima's settings to load other extensions on home screen)"
+    authors = listOf("RowdyRushya")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1
+
+    tvTypes = listOf("All")
+
+    requiresResources = true
+    language = "en"
+
+    // random cc logo i found
+    iconUrl = "https://raw.githubusercontent.com/Rowdy-Avocado/Rowdycado-Extensions/master/logos/ultima.png"
 }
 
 android {
-    buildFeatures {
-        buildConfig = true
-    }
-
     defaultConfig {
-        val apiKey = project.findProperty("tmdbApiKey")?.toString() ?: ""
-        buildConfigField("String", "TMDB_SECRET_API", "\"$apiKey\"")
+        val properties = Properties()
+        properties.load(project.rootProject.file("local.properties").inputStream())
+
+        buildConfigField("String", "SIMKL_API", "\"${properties.getProperty("SIMKL_API")}\"")
+        buildConfigField("String", "MAL_API", "\"${properties.getProperty("MAL_API")}\"")
     }
-}
-
-cloudstream {
-    authors     = listOf("GitLatte", "patr0nq", "keyiflerolsun")
-    language    = "tr"
-    description = "powerboard`un yabancı dizi arşivi"
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-    **/
-    status  = 1 // will be 3 if unspecified
-    tvTypes = listOf("TvSeries")
-    iconUrl = "https://raw.githubusercontent.com/GitLatte/Sinetech/master/img/powerdizi/powerdizi.png"
 }
