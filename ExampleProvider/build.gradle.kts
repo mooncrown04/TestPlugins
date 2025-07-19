21e6b5345de91c26da54e6daa3569143ad164a43
mooncrown04 <mooncrown04@gmail.com>
2025-07-19 16:35:00 +0300

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,47 +1,38 @@
-// TestPlugins/src/ExampleProvider/build.gradle.kts
-
-version = 3
-
 dependencies {
-    // Tüm bağımlılıkları parantez () içine alın!
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+}
+// use an integer for version numbers
+version = -1
 
-    // AndroidX UI ve yardımcı kütüphaneler
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
 
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.fragment:fragment-ktx:1.7.1")
+cloudstream {
+    // All of these properties are optional, you can safely remove them
 
-    implementation("androidx.annotation:annotation:1.8.0")
+    description = "Lorem ipsum"
+    authors = listOf("Cloudburst")
 
-    // Buraya projenizdeki diğer bağımlılıkları ekleyebilirsiniz
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1
+
+    tvTypes = listOf("Movie")
+
+    requiresResources = true
+    language = "en"
+
+    // random cc logo i found
+    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
 }
 
 android {
     buildFeatures {
-        buildConfig = true
+        viewBinding = true
     }
-
-    defaultConfig {
-        val apiKey = project.findProperty("tmdbApiKey")?.toString() ?: ""
-        buildConfigField("String", "TMDB_SECRET_API", "\"$apiKey\"")
-    }
-}
-
-cloudstream {
-    authors       = listOf("GitLatte", "patr0nq", "keyiflerolsun")
-    language      = "tr"
-    description = "powerboard`un sinema arşivi"
-
-    /**
-     * Durum int'i aşağıdaki gibidir:
-     * 0: Kapalı
-     * 1: Tamam
-     * 2: Yavaş
-     * 3: Sadece Beta
-     **/
-    status  = 1 // belirtilmezse 3 olur
-    tvTypes = listOf("Movie")
-    iconUrl = "https://raw.githubusercontent.com/GitLatte/Sinetech/master/img/powersinema/powersinema.png"
 }
