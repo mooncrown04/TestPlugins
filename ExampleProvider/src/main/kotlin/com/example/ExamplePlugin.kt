4c6e7a491e2a73dd56298e73004334ae3ec87f1f
mooncrown04 <mooncrown04@gmail.com>
2025-07-19 16:36:51 +0300

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -2,12 +2,23 @@ package com.example
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.APIHolder
 import android.content.Context
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 @CloudstreamPlugin
-class ExamplePlugin : Plugin() {
+class TestPlugin: Plugin() {
+    var activity: AppCompatActivity? = null
+
     override fun load(context: Context) {
-        // All providers should be added in this manner. Please don't edit the following list manually.
-        registerMainAPI(powerSinema(context, null))
+        activity = context as AppCompatActivity
+        // All providers should be added in this manner
+        registerMainAPI(ExampleProvider(this))
+
+        openSettings = { ctx ->
+            val frag = BlankFragment(this)
+            frag.show(activity!!.supportFragmentManager, "Frag")
+        }
     }
 }
