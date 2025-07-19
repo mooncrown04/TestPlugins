df99231fc99b344556c9777967aa76b2dac7f0e1
mooncrown04 <mooncrown04@gmail.com>
2025-07-19 22:37:17 +0300

    Create FilmPlugin.kt

---
+++ Sinema/src/main/kotlin/com/mooncrown/FilmPlugin.kt
@@ -0,0 +1,13 @@
+package com.mooncrown
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class FilmPlugin : Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the following list manually.
+        registerMainAPI(powerSinema(context, null))
+    }
+}
