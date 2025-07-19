a2b6af4f15654cd63328be685a97392c72f747d9
mooncrown04 <mooncrown04@gmail.com>
2025-07-19 20:39:24 +0300

    Update FilmPlugin.kt

--- SinemaM3u/src/main/kotlin/com/mooncrown/FilmPlugin.kt
+++ SinemaM3u/src/main/kotlin/com/mooncrown/FilmPlugin.kt
@@ -1,1 +1,13 @@
-jjjj
+package com.film
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+import com.film.Film
+@CloudstreamPlugin
+class FilmPlugin : Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the following list manually.
+        registerMainAPI(Film(context, null))
+    }
+}
