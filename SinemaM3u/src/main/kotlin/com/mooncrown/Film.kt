d563b5a6a236d338b4b9cadaa6df91f64977d800
mooncrown04 <mooncrown04@gmail.com>
2025-07-19 20:50:17 +0300

    Update Film.kt

--- SinemaM3u/src/main/kotlin/com/mooncrown/Film.kt
+++ SinemaM3u/src/main/kotlin/com/mooncrown/Film.kt
@@ -7,7 +7,7 @@ import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
 import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import java.io.InputStream
-import com.film.BuildConfig
+import com.mooncrown.BuildConfig
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.json.JSONObject
