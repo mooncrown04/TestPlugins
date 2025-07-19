980f6681fec03047db17dc17fda2c2d88e2f8ca3
mooncrown04 <mooncrown04@gmail.com>
2025-07-19 20:13:14 +0300

    Update build.gradle.kts

--- SinemaM3u/build.gradle.kts
+++ SinemaM3u/build.gradle.kts
@@ -1,7 +1,21 @@
+// TestPlugins/src/SinemaM3u/build.gradle.kts
+
 version = 3
 
 dependencies {
+    // Tüm bağımlılıkları parantez () içine alın!
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    // AndroidX UI ve yardımcı kütüphaneler
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.fragment:fragment-ktx:1.7.1")
+
+    implementation("androidx.annotation:annotation:1.8.0")
+
+    // Buraya projenizdeki diğer bağımlılıkları ekleyebilirsiniz
 }
 
 android {
@@ -16,18 +30,18 @@ android {
 }
 
 cloudstream {
-    authors     = listOf("GitLatte", "patr0nq", "keyiflerolsun")
-    language    = "tr"
-    description = "powerboard`un yabancı dizi arşivi"
+    authors       = listOf("GitLatte", "patr0nq", "keyiflerolsun")
+    language      = "tr"
+    description = "powerboard`un sinema arşivi"
 
     /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-    **/
-    status  = 1 // will be 3 if unspecified
-    tvTypes = listOf("TvSeries")
-    iconUrl = "https://raw.githubusercontent.com/GitLatte/Sinetech/master/img/powerdizi/powerdizi.png"
+     * Durum int'i aşağıdaki gibidir:
+     * 0: Kapalı
+     * 1: Tamam
+     * 2: Yavaş
+     * 3: Sadece Beta
+     **/
+    status  = 1 // belirtilmezse 3 olur
+    tvTypes = listOf("Movie")
+    iconUrl = "https://raw.githubusercontent.com/GitLatte/Sinetech/master/img/powersinema/powersinema.png"
 }
