bd21f0480ab04b6cbc3426a1b4ab77dcb7819728
mooncrown04 <mooncrown04@gmail.com>
2025-07-20 16:19:47 +0300

    Update PortalApiPlugin.kt

--- Maciptv/src/main/kotlin/com/moncrown/PortalApiPlugin.kt
+++ Maciptv/src/main/kotlin/com/moncrown/PortalApiPlugin.kt
@@ -4,9 +4,14 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
-@CloudstreamPlugin
+@Plugin(
+    id = "com.mooncrown.PortalApi", // Eklentiniz için benzersiz bir kimlik
+    version = "1.0.0", // Eklentinizin versiyonu
+    displayName = "MoOnCrOwN" // Cloudstream'de görünecek adı
+)
 class PortalApiPlugin: Plugin() {
     override fun load(context: Context) {
+        // PortalApiProvider'ı Cloudstream'e kaydeder
         registerMainAPI(PortalApiProvider())
     }
 }
