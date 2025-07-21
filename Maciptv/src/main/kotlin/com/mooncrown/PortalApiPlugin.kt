5d3586be1e83aa836cd0927cffba6855a9d78269
mooncrown04 <mooncrown04@gmail.com>
2025-07-21 17:35:47 +0300

    Update PortalApiPlugin.kt

--- Maciptv/src/main/kotlin/com/mooncrown/PortalApiPlugin.kt
+++ Maciptv/src/main/kotlin/com/mooncrown/PortalApiPlugin.kt
@@ -1,35 +1,12 @@
 package com.mooncrown
 
-import android.content.Context
-import com.lagradost.cloudstream3.plugins.Plugin // Doğru Plugin import'u
-import com.lagradost.cloudstream3.AcraApplication // AcraApplication import'u
-import com.lagradost.cloudstream3.mvvm.logError // logError import'u
-import androidx.appcompat.app.AppCompatActivity // AppCompatActivity import'u
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.plugin.Plugin
+import com.lagradost.cloudstream3.plugin.PluginManager
 
-// Bu sınıf, Cloudstream uygulamasının bir eklentisi olarak kaydedilir.
-// Tüm sağlayıcılarınızı (MainAPI'lerinizi) buradan kaydedersiniz.
-@Plugin(
-    id = "com.mooncrown.Maciptv", // Eklentinizin benzersiz kimliği
-    version = "1.0.0", // Eklentinizin versiyonu
-    displayName = "Maciptv Plugin" // Eklentinizin görünen adı
-)
-class PortalApiPlugin : Plugin() { // Plugin sınıfından türetilmeli
-
-    override fun load() {
-        // PortalApiProvider'ı bir MainAPI olarak kaydediyoruz.
-        // Bu, Cloudstream'in bu sağlayıcıyı tanımasını sağlar.
+@Plugin
+class PortalApiPlugin : Plugin() {
+    override fun load(context: Context) {
         registerMainAPI(PortalApiProvider())
-
-        // Ayarlar veya diğer eklentiye özgü işlevler buraya eklenebilir.
-        // Örneğin, eğer PortalApiProvider'da bir ayar menüsü açmak istiyorsanız:
-        // addKey(R.string.portal_api_settings_key) { // R.string.portal_api_settings_key tanımlı olmalı
-        //     val ctx = AcraApplication.context
-        //     if (ctx is AppCompatActivity) {
-        //         // Ayarlar fragment'ınızı burada gösterin
-        //         // Örneğin: PortalApiSettingsFragment().show(ctx.supportFragmentManager, "PortalApiSettings")
-        //     } else {
-        //         logError(IllegalStateException("Context AppCompatActivity değil, ayarlar gösterilemiyor."))
-        //     }
-        // }
     }
 }
