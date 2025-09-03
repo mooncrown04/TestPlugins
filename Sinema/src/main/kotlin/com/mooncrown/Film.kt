238b8c069853cafba44b78ead27e4ff61bba6ea9
mooncrown04 <mooncrown04@gmail.com>
2025-09-04 02:01:24 +0300

    Update Film.kt

--- Sinema/src/main/kotlin/com/mooncrown/Film.kt
+++ Sinema/src/main/kotlin/com/mooncrown/Film.kt
@@ -16,8 +16,9 @@ import java.net.URLEncoder
 
 class powerSinema(private val context: android.content.Context, private val sharedPref: SharedPreferences?) : MainAPI() {
 
-    override var mainUrl              = "https://raw.githubusercontent.com/emrcxcx/test/refs/heads/main/filmfun.m3u"
-    override var name                 = "A Sinema 🎥"
+ //   override var mainUrl              = "https://raw.githubusercontent.com/emrcxcx/test/refs/heads/main/filmfun.m3u"
+  override var mainUrl              = "https://raw.githubusercontent.com/mooncrown04/mooncrown34/refs/heads/master/dizi.m3u"
+    override var name                 = "35 Sinema 🎥"
     override val hasMainPage          = true
     override var lang                 = "tr"
     override val hasQuickSearch       = true
