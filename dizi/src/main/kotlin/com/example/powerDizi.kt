47cb456309d52071d9875f286364a838339d5634
mooncrown04 <mooncrown04@gmail.com>
2025-09-03 22:28:15 +0300

    Update powerDizi.kt

--- dizi/src/main/kotlin/com/example/powerDizi.kt
+++ dizi/src/main/kotlin/com/example/powerDizi.kt
@@ -461,4 +461,204 @@ data class PlaylistItem(
     val url: String? = null,
     val userAgent: String? = null,
     val season: Int = 1,
-    val episode: Int =
+    val episode: Int = 0
+) {
+    companion object {
+        const val EXT_M3U = "#EXTM3U"
+        const val EXT_INF = "#EXTINF"
+        const val EXT_VLC_OPT = "#EXTVLCOPT"
+    }
+}
+
+class IptvPlaylistParser {
+
+    /**
+     * Parse M3U8 string into [Playlist]
+     *
+     * @param content M3U8 content string.
+     * @throws PlaylistParserException if an error occurs.
+     */
+    fun parseM3U(content: String): Playlist {
+        return parseM3U(content.byteInputStream())
+    }
+
+    /**
+     * Parse M3U8 content [InputStream] into [Playlist]
+     *
+     * @param input Stream of input data.
+     * @throws PlaylistParserException if an error occurs.
+     */
+    @Throws(PlaylistParserException::class)
+    fun parseM3U(input: InputStream): Playlist {
+        val reader = input.bufferedReader()
+
+        if (!reader.readLine().isExtendedM3u()) {
+            throw PlaylistParserException.InvalidHeader()
+        }
+
+        val EXT_M3U = PlaylistItem.EXT_M3U
+        val EXT_INF = PlaylistItem.EXT_INF
+        val EXT_VLC_OPT = PlaylistItem.EXT_VLC_OPT
+
+        val playlistItems: MutableList<PlaylistItem> = mutableListOf()
+        var currentIndex = 0
+
+        var line: String? = reader.readLine()
+
+        while (line != null) {
+            if (line.isNotEmpty()) {
+                if (line.startsWith(EXT_INF)) {
+                    val title = line.getTitle()
+                    val attributes = line.getAttributes()
+
+                    playlistItems.add(PlaylistItem(title, attributes))
+                } else if (line.startsWith(EXT_VLC_OPT)) {
+                    val item = playlistItems[currentIndex]
+                    val userAgent = item.userAgent ?: line.getTagValue("http-user-agent")?.toString()
+                    val referrer = line.getTagValue("http-referrer")?.toString()
+
+                    val headers = mutableMapOf<String, String>()
+
+                    if (userAgent != null) {
+                        headers["user-agent"] = userAgent
+                    }
+
+                    if (referrer != null) {
+                        headers["referrer"] = referrer
+                    }
+
+                    playlistItems[currentIndex] = item.copy(
+                        userAgent = userAgent,
+                        headers = headers
+                    )
+                } else {
+                    if (!line.startsWith("#")) {
+                        val item = playlistItems[currentIndex]
+                        val url = line.getUrl()
+                        val userAgent = line.getUrlParameter("user-agent")
+                        val referrer = line.getUrlParameter("referer")
+                        val urlHeaders = if (referrer != null) {item.headers + mapOf("referrer" to referrer)} else item.headers
+
+                        playlistItems[currentIndex] = item.copy(
+                            url = url,
+                            headers = item.headers + urlHeaders,
+                            userAgent = userAgent ?: item.userAgent
+                        )
+                        currentIndex++
+                    }
+                }
+            }
+
+            line = reader.readLine()
+        }
+        return Playlist(playlistItems)
+    }
+
+    private fun String.replaceQuotesAndTrim(): String {
+        return replace("\"", "").trim()
+    }
+
+    private fun String.isExtendedM3u(): Boolean = startsWith(PlaylistItem.EXT_M3U)
+
+    private fun String.getTitle(): String? {
+        return split(",").lastOrNull()?.replaceQuotesAndTrim()
+    }
+
+    private fun String.getUrl(): String? {
+        return split("|").firstOrNull()?.replaceQuotesAndTrim()
+    }
+
+    private fun String.getUrlParameter(key: String): String? {
+        val urlRegex = Regex("^(.*)\\|", RegexOption.IGNORE_CASE)
+        val keyRegex = Regex("$key=(\\w[^&]*)", RegexOption.IGNORE_CASE)
+        val paramsString = replace(urlRegex, "").replaceQuotesAndTrim()
+
+        return keyRegex.find(paramsString)?.groups?.get(1)?.value
+    }
+
+    private fun String.getTagValue(key: String): String? {
+        val keyRegex = Regex("$key=(.*)", RegexOption.IGNORE_CASE)
+        return keyRegex.find(this)?.groups?.get(1)?.value?.replaceQuotesAndTrim()
+    }
+
+    private fun String.getAttributes(): Map<String, String> {
+        val extInfRegex = Regex("(#EXTINF:.?[0-9]+)", RegexOption.IGNORE_CASE)
+        val attributesString = replace(extInfRegex, "").replaceQuotesAndTrim()
+        val titleAndAttributes = attributesString.split(",", limit = 2)
+        
+        val attributes = mutableMapOf<String, String>()
+        if (titleAndAttributes.size > 1) {
+            val attrRegex = Regex("([\\w-]+)=\"([^\"]*)\"|([\\w-]+)=([^\"]+)")
+            
+            attrRegex.findAll(titleAndAttributes[0]).forEach { matchResult ->
+                val (quotedKey, quotedValue, unquotedKey, unquotedValue) = matchResult.destructured
+                val key = quotedKey.takeIf { it.isNotEmpty() } ?: unquotedKey
+                val value = quotedValue.takeIf { it.isNotEmpty() } ?: unquotedValue
+                attributes[key] = value.replaceQuotesAndTrim()
+            }
+        }
+
+        if (!attributes.containsKey("tvg-country")) {
+            attributes["tvg-country"] = "TR/Altyazılı"
+        }
+        if (!attributes.containsKey("tvg-language")) {
+            attributes["tvg-language"] = "TR/Altyazılı"
+        }
+        if (!attributes.containsKey("group-title")) {
+            val (cleanTitle, _, _) = parseEpisodeInfo(titleAndAttributes.last())
+            attributes["group-title"] = cleanTitle
+        }
+
+        return attributes
+    }
+}
+
+sealed class PlaylistParserException(message: String) : Exception(message) {
+    class InvalidHeader : PlaylistParserException("Invalid file header. Header doesn't start with #EXTM3U")
+}
+
+val languageMap = mapOf(
+    "en" to "İngilizce",
+    "tr" to "Türkçe",
+    "ja" to "Japonca",
+    "de" to "Almanca",
+    "fr" to "Fransızca",
+    "es" to "İspanyolca",
+    "it" to "İtalyanca",
+    "ru" to "Rusça",
+    "pt" to "Portekizce",
+    "ko" to "Korece",
+    "zh" to "Çince",
+    "hi" to "Hintçe",
+    "ar" to "Arapça",
+    "nl" to "Felemenkçe",
+    "sv" to "İsveççe",
+    "no" to "Norveççe",
+    "da" to "Danca",
+    "fi" to "Fince",
+    "pl" to "Lehçe",
+    "cs" to "Çekçe",
+    "hu" to "Macarca",
+    "ro" to "Rumence",
+    "el" to "Yunanca",
+    "uk" to "Ukraynaca",
+    "bg" to "Bulgarca",
+    "sr" to "Sırpça",
+    "hr" to "Hırvatça",
+    "sk" to "Slovakça",
+    "sl" to "Slovence",
+    "th" to "Tayca",
+    "vi" to "Vietnamca",
+    "id" to "Endonezce",
+    "ms" to "Malayca",
+    "tl" to "Tagalogca",
+    "fa" to "Farsça",
+    "he" to "İbranice",
+    "la" to "Latince",
+    "xx" to "Belirsiz",
+    "mul" to "Çok Dilli"
+)
+
+fun getTurkishLanguageName(code: String?): String? {
+    return languageMap[code?.lowercase()]
+}
