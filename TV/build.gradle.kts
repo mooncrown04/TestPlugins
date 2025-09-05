98b910d12afbd748d5be61c5a5fce18b7b5ed5ca
mooncrown04 <mooncrown04@gmail.com>
2025-09-05 21:08:57 +0300

    Update build.gradle.kts

--- TV/build.gradle.kts
+++ TV/build.gradle.kts
@@ -1,33 +1,46 @@
-version = 3
-
-dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.lagradost.cloudstream3.plugin")
 }
 
 android {
-    buildFeatures {
-        buildConfig = true
-    }
+    namespace = "com.MoOnCrOwNTV"
+    compileSdkVersion(34)
 
     defaultConfig {
+        minSdkVersion(21)
+        targetSdkVersion(34)
+
         val apiKey = project.findProperty("tmdbApiKey")?.toString() ?: ""
         buildConfigField("String", "TMDB_SECRET_API", "\"$apiKey\"")
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    // Bu satır, hatanın kaynağı olan kütüphaneyi ekler.
+    implementation("com.lagradost.cloudstream3:cloudstream3-api:latest.integration")
 }
 
 cloudstream {
-    authors     = listOf("MoOnCrOwN","GitLatte", "patr0nq", "keyiflerolsun")
-    language    = "tr"
+    authors = listOf("MoOnCrOwN", "GitLatte", "patr0nq", "keyiflerolsun")
+    language = "tr"
     description = "MoOnCrOwN TV"
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-    **/
-    status  = 1 // will be 3 if unspecified
+    status = 1
     tvTypes = listOf("Live")
     iconUrl = "https://raw.githubusercontent.com/GitLatte/Sinetech/master/img/powerdizi/powerdizi.png"
 }
