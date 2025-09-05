9c01923770e62599d8dd624d8f16e8ab235d3b64
mooncrown04 <mooncrown04@gmail.com>
2025-09-06 00:31:47 +0300

    Update MoOnCrOwNTV.kt

--- TV/src/main/kotlin/com/MoOnCrOwNTV/MoOnCrOwNTV.kt
+++ TV/src/main/kotlin/com/MoOnCrOwNTV/MoOnCrOwNTV.kt
@@ -8,8 +8,8 @@ import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import java.io.InputStream
 
 class MoOnCrOwNTV : MainAPI() {
-    override var mainUrl              = "https://raw.githubusercontent.com/feroxx/test/refs/heads/main/Kanallar/canlitv.m3u"
-    override var name                 = "CanliTV"
+    override var mainUrl              = "https://dl.dropbox.com/scl/fi/r4p9v7g76ikwt8zsyuhyn/sile.m3u?rlkey=esnalbpm4kblxgkvym51gjokm"
+    override var name                 = "35 MoOnCrOwN TV"
     override val hasMainPage          = true
     override var lang                 = "tr"
     override val hasQuickSearch       = true
