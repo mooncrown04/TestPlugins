0c2895abfb302fb2120d870a9d0e0c31a156ad36
mooncrown04 <mooncrown04@gmail.com>
2025-09-05 20:56:41 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,13 +19,7 @@ buildscript {
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-}
+
 
 fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
 
