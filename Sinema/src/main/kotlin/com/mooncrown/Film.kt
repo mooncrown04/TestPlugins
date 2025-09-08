279b63608465a04ebb47b5b3d66ad691bd7b7f85
mooncrown04 <mooncrown04@gmail.com>
2025-09-09 02:33:23 +0300

    Update Film.kt

--- Sinema/src/main/kotlin/com/mooncrown/Film.kt
+++ Sinema/src/main/kotlin/com/mooncrown/Film.kt
@@ -14,10 +14,11 @@ import org.json.JSONObject
 import java.net.URL
 import java.net.URLEncoder
 
+// Gerekli importları ekledik
 import com.lagradost.cloudstream3.DubStatus
 import com.lagradost.cloudstream3.addDubStatus
 
-class Film(private val context: android.content.Context, private val sharedPref: SharedPreferences?) : MainAPI() {
+class powerSinema(private val context: android.content.Context, private val sharedPref: SharedPreferences?) : MainAPI() {
     override var mainUrl = "https://raw.githubusercontent.com/mooncrown04/mooncrown34/refs/heads/master/dizi.m3u"
     override var name = "35 Anime 📺"
     override val hasMainPage = true
