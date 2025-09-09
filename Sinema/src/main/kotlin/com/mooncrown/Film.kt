b6d0c18a6e8a66ab482c9fcd8669d128e75de946
mooncrown04 <mooncrown04@gmail.com>
2025-09-09 03:08:49 +0300

    Update Film.kt

--- Sinema/src/main/kotlin/com/mooncrown/Film.kt
+++ Sinema/src/main/kotlin/com/mooncrown/Film.kt
@@ -296,13 +296,14 @@ class Film(private val context: android.content.Context, private val sharedPref:
             }
         }
 
-        return newAnimeLoadResponse(displayTitle, url, TvType.Anime, loadData.url) {
-            this.posterUrl = loadData.poster
-            this.plot = plot
-            this.recommendations = recommendations
-            this.rating = (tmdbData?.optDouble("vote_average", 0.0)?.toFloat()?.times(2)?.toInt() ?: (if (isWatched) 5 else 0))
-            this.duration = if (watchProgress > 0) (watchProgress / 1000).toInt() else tmdbData?.optInt("runtime", 0)
-            this.comingSoon = false
+   return newAnimeLoadResponse(displayTitle, url, TvType.Anime, false) {
+    this.posterUrl = loadData.poster
+    this.plot = plot
+    this.recommendations = recommendations
+    this.rating = (tmdbData?.optDouble("vote_average", 0.0)?.toFloat()?.times(2)?.toInt() ?: (if (isWatched) 5 else 0))
+    this.duration = if (watchProgress > 0) (watchProgress / 1000).toInt() else tmdbData?.optInt("runtime", 0)
+    this.comingSoon = false
+
         }
     }
 
