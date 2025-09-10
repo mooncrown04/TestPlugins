6adb79ddd6c94b862c3335082b3832cd9b66415b
mooncrown04 <mooncrown04@gmail.com>
2025-09-10 17:16:43 +0300

    Update AnimeDizi.kt

--- Anime/src/main/kotlin/com/example/AnimeDizi.kt
+++ Anime/src/main/kotlin/com/example/AnimeDizi.kt
@@ -124,7 +124,7 @@ fun parseEpisodeInfo(text: String): Triple<String, Int?, Int?> {
 // --- Ana Eklenti Sınıfı ---
 class AnimeDizi(private val sharedPref: SharedPreferences?) : MainAPI() {
      //override var mainUrl = "https://raw.githubusercontent.com/mooncrown04/mooncrown34/refs/heads/master/dizi.m3u"
-    override var mainUrl = "https://dl.dropbox.com/scl/fi/getue3cktknyt34rqebuu/MoOnCrOwN35dizi.m3u?rlkey=sl5r2zcudctxa5691h9g5iilc"
+    override var mainUrl = "https://dl.dropbox.com/scl/fi/piul7441pe1l41qcgq62y/powerdizi.m3u?rlkey=zwfgmuql18m09a9wqxe3irbbr"
     override var name = "35 Anime-Dizi 🎬"
     override val hasMainPage = true
     override var lang = "tr"
