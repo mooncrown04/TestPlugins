9837deb036533d06389a6d594a7d9cc2e919e987
mooncrown04 <mooncrown04@gmail.com>
2025-09-16 19:37:26 +0300

    Update build.gradle.kts

--- Anime/build.gradle.kts
+++ Anime/build.gradle.kts
@@ -1,29 +1,32 @@
-plugins {
-    id("com.android.library") // veya "com.android.application" ihtiyacınıza göre
-    kotlin("android")
-}
+import java.util.Properties
+import java.io.FileInputStream
 
 android {
-    buildFeatures {
-        buildConfig = true
-    }
-
     defaultConfig {
-        val apiKey = project.findProperty("tmdbApiKey")?.toString() ?: ""
+        val localProps = Properties()
+        val localFile = rootProject.file("local.properties")
+        if (localFile.exists()) {
+            localProps.load(FileInputStream(localFile))
+        }
+
+        val apiKey = localProps.getProperty("tmdbApiKey") ?: ""
         buildConfigField("String", "TMDB_SECRET_API", "\"$apiKey\"")
     }
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("com.lagradost.cloudstream3:cloudstream-tmdb:1.0.0")
 }
 
 cloudstream {
     authors     = listOf("GitLatte", "patr0nq", "keyiflerolsun")
     language    = "tr"
     description = "yabancı anime arşivi"
-    status      = 1
-    tvTypes     = listOf("TvSeries")
-    iconUrl     = "https://raw.githubusercontent.com/GitLatte/Sinetech/master/img/powerdizi/powerdizi.png"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("TvSeries")
+    iconUrl = "https://raw.githubusercontent.com/GitLatte/Sinetech/master/img/powerdizi/powerdizi.png"
 }
