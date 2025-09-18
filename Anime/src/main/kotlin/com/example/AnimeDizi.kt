0b153c4b07b927d5eadfba0e5534604db06899b9
mooncrown04 <mooncrown04@gmail.com>
2025-09-18 20:16:42 +0300

    Update AnimeDizi.kt

--- Anime/src/main/kotlin/com/example/AnimeDizi.kt
+++ Anime/src/main/kotlin/com/example/AnimeDizi.kt
@@ -20,7 +20,7 @@ import com.lagradost.cloudstream3.Score
 // --- Ana Eklenti Sınıfı ---
 class AnimeDizi(private val sharedPref: SharedPreferences?) : MainAPI() {
     override var mainUrl = "https://dl.dropbox.com/scl/fi/piul7441pe1l41qcgq62y/powerdizi.m3u?rlkey=zwfgmuql18m09a9wqxe3irbbr"
-    override var name = "35 Anime Diziler  🎬"
+    override var name = "35 Animesi Diziler  🎬"
     override val hasMainPage = true
     override var lang = "tr"
     override val hasQuickSearch = true
@@ -140,6 +140,8 @@ fun parseEpisodeInfo(text: String): Triple<String, Int?, Int?> {
     val format1Regex = Regex("""(.*?)[^\w\d]+(\d+)\.\s*Sezon\s*(\d+)\.\s*Bölüm.*""", RegexOption.IGNORE_CASE)
     val format2Regex = Regex("""(.*?)\s*s(\d+)e(\d+)""", RegexOption.IGNORE_CASE)
     val format3Regex = Regex("""(.*?)\s*Sezon\s*(\d+)\s*Bölüm\s*(\d+).*""", RegexOption.IGNORE_CASE)
+    val format4Regex = Regex("""(.*?)\s*(\d+)\s*Bölüm.*""", RegexOption.IGNORE_CASE)
+
 
     val matchResult1 = format1Regex.find(textWithCleanedChars)
     if (matchResult1 != null) {
@@ -157,6 +159,11 @@ fun parseEpisodeInfo(text: String): Triple<String, Int?, Int?> {
     if (matchResult3 != null) {
         val (title, seasonStr, episodeStr) = matchResult3.destructured
         return Triple(title.trim(), seasonStr.toIntOrNull(), episodeStr.toIntOrNull())
+    }
+    val matchResult4 = format4Regex.find(textWithCleanedChars)
+    if (matchResult4 != null) {
+        val (title, episodeStr) = matchResult4.destructured
+        return Triple(title.trim(), 1, episodeStr.toIntOrNull())
     }
 
     return Triple(textWithCleanedChars.trim(), null, null)
