d5e03b16626990a3d7f8901b830c088f0fdc4900
mooncrown04 <mooncrown04@gmail.com>
2025-09-18 22:53:54 +0300

    Update AnimeDiziPlugin.kt

--- Anime/src/main/kotlin/com/example/AnimeDiziPlugin.kt
+++ Anime/src/main/kotlin/com/example/AnimeDiziPlugin.kt
@@ -2,16 +2,10 @@ package com.example
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-//import android.content.Context
 
 @CloudstreamPlugin
 class AnimeDiziPlugin: Plugin() {
-//    override fun load(context: Context) {
- //   registerMainAPI(AnimeDizi(context.getSharedPreferences("AnimeDizi", 0)))
- override fun load(context: CloudstreamPlugin) {
+    override fun load(context: CloudstreamPlugin) {
         registerMainAPI(AnimeDizi(context))
     }
 }
-
-
- 
