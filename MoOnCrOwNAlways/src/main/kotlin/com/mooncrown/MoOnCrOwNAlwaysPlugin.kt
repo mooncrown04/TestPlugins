c2d5a2294ee70a7590df473a953def0a1fa65d73
mooncrown04 <mooncrown04@gmail.com>
2025-09-22 16:34:08 +0300

    eklendi.

---
+++ MoOnCrOwNAlways/src/main/kotlin/com/mooncrown/MoOnCrOwNAlwaysPlugin.kt
@@ -0,0 +1,13 @@
+package com.mooncrown
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class MoOnCrOwNAlwaysPlugin: Plugin() {
+    override fun load(context: Context) {
+     //  registerMainAPI(MoOnCrOwNAlways(context.getSharedPreferences("MoOnCrOwNAlways", 0)))
+  registerMainAPI(MoOnCrOwNAlways(context, null))
+    }
+}
