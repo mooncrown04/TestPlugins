93541e6db1451b51533d77d694c65c0e89b80058
mooncrown04 <mooncrown04@gmail.com>
2025-09-27 01:10:48 +0300

    Update MoOnCrOwNAlwaysPlugin.kt

--- MoOnCrOwNAlways/src/main/kotlin/com/mooncrown/MoOnCrOwNAlwaysPlugin.kt
+++ MoOnCrOwNAlways/src/main/kotlin/com/mooncrown/MoOnCrOwNAlwaysPlugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class AnimeDiziPlugin: Plugin() {
+class MoOnCrOwNAlwaysPlugin: Plugin() {
     override fun load(context: Context) {
     registerMainAPI(AnimeDizi(context.getSharedPreferences("AnimeDizi", 0)))
  
