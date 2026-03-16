62fb44163d69f0f25a5b3663e3cee8a8a45d6084
mooncrown04 <mooncrown04@gmail.com>
2026-03-16 20:33:07 +0300

    Update build.gradle.kts

--- mooncrowndeneme/build.gradle.kts
+++ mooncrowndeneme/build.gradle.kts
@@ -4,11 +4,13 @@ buildscript {
     repositories {
         google()
         mavenCentral()
+        // JitPack alternatifleri ve ana repo
         maven("https://jitpack.io")
+        maven("https://maven.pkg.github.com/LagradOst/CloudStream-Releases")
     }
     dependencies {
-        // Jitpack 401 hatasını aşmak için sabit commit ID kullanıyoruz
-        classpath("com.github.LagradOst:CloudStream-Gradle-Plugin:f1134267e7")
+        // En güncel ve stabil SNAPSHOT yerine doğrudan sürüm aramaya zorlayalım
+        classpath("com.github.LagradOst:CloudStream-Gradle-Plugin:master-SNAPSHOT")
     }
 }
 
@@ -16,47 +18,30 @@ apply(plugin = "com.android.library")
 apply(plugin = "kotlin-android")
 apply(plugin = "com.lagradost.cloudstream3")
 
-// Proje Versiyonu
-version = 1 
-
 cloudstream {
-    // metadata ayarları
     authors     = listOf("MoOnCrOwN")
     language    = "tr"
     description = "GitHub üzerinden dinamik olarak güncellenen Mooncrown sinema arşivi."
-
-    status  = 1 // 1: Beta, 2: Stable
-    tvTypes = listOf("Movie", "TvSeries") 
-    
-    iconUrl = "https://raw.githubusercontent.com/mooncrown04/TestPlugins/master/icon.png"
+    status      = 1
+    tvTypes     = listOf("Movie", "TvSeries")
+    iconUrl     = "https://raw.githubusercontent.com/mooncrown04/TestPlugins/master/icon.png"
 }
 
 android {
-    namespace = "com.mooncrown.deneme" // Benzersiz bir paket adı
+    namespace = "com.mooncrown.deneme"
     compileSdk = 34
     
     defaultConfig {
         minSdk = 21
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
-    val cloudstreamVersion = "3.0.0"
-    
-    // Temel Cloudstream kütüphaneleri
-    compileOnly("com.github.LagradOst:CloudStream:$cloudstreamVersion")
+    // Cloudstream kütüphanesini 'compileOnly' yaparak ana uygulama ile çakışmasını önleyelim
+    compileOnly("com.github.LagradOst:CloudStream:3.0.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     
-    // JS ve JSON işleme için gerekli bağımlılıklar
+    // JS motoru ve veri işleme
     implementation("com.lagradost:ducktape:1.0.4")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
 }
