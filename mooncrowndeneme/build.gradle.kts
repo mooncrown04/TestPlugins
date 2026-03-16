7257fc3038ee8c6b6594ff953b9eb7933815c7a7
mooncrown04 <mooncrown04@gmail.com>
2026-03-16 20:15:17 +0300

    Update build.gradle.kts

--- mooncrowndeneme/build.gradle.kts
+++ mooncrowndeneme/build.gradle.kts
@@ -1,41 +1,50 @@
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 
-// Plugins bloğu (Eğer dosyanın başında varsa kalsın)
-plugins {
-    id("com.android.library")
-    kotlin("android")
-    // Buraya versiyon numarasını ekliyoruz:
-    id("com.lagradost.cloudstream3") version "0.1.0" 
+// 1. REPOLARI VE CLASSPATH'İ EL İLE TANIMLIYORUZ
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        // En güncel sürümü açıkça belirtiyoruz
+        classpath("com.github.LagradOst:CloudStream-Gradle-Plugin:master-SNAPSHOT")
+    }
 }
 
-// Eklentinin versiyonu.
+// 2. PLUGINLERİ UYGULUYORUZ (id yerine apply kullanıyoruz)
+apply(plugin = "com.android.library")
+apply(plugin = "kotlin-android")
+apply(plugin = "com.lagradost.cloudstream3")
+
 version = 1 
 
 cloudstream {
-    // Kendi adını veya ekibini yazabilirsin
     authors     = listOf("MoOnCrOwN") 
     language    = "tr"
     description = "GitHub üzerinden dinamik olarak güncellenen Mooncrown sinema arşivi."
 
     status  = 1 
-    // İleride dizi eklemek istersen TvSeries'i buraya ekledim, derleme yapmana gerek kalmaz.
     tvTypes = listOf("Movie", "TvSeries") 
     
     iconUrl = "https://raw.githubusercontent.com/mooncrown04/TestPlugins/master/icon.png"
 }
 
 android {
-    // Mevcut android ayarların (compileSdk vb.) burada kalmalı
-    namespace = "com.example" // Paket adınla uyumlu olmalı
+    namespace = "com.example"
+    compileSdk = 34
+    
+    defaultConfig {
+        minSdk = 21
+    }
 }
 
 dependencies {
-    val cloudstreamVersion = "3.0.0" // Kullandığın SDK versiyonuna göre değişebilir
-    
-    // Temel Cloudstream kütüphaneleri
-    implementation("com.lagradost:cloudstream3:$cloudstreamVersion")
+    // Cloudstream SDK'nın kendisi
+    implementation("com.github.LagradOst:CloudStream:3.0.0") 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     
-    // KRİTİK: GitHub'dan gelen JS kodunu çalıştırmak için gereken motor
+    // JS motoru (Loglardaki hatayı çözen kısım)
     implementation("com.lagradost:ducktape:1.0.4")
 }
