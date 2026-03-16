d40e6a811963c5f144b28c9591df17b2c6d034eb
mooncrown04 <mooncrown04@gmail.com>
2026-03-16 21:17:26 +0300

    Update build.gradle.kts

--- mooncrowndeneme/build.gradle.kts
+++ mooncrowndeneme/build.gradle.kts
@@ -7,12 +7,11 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        // master-SNAPSHOT yerine çalışan stabil commit
-        classpath("com.github.LagradOst:CloudStream-Gradle-Plugin:f0f1c4293a")
+        // Hash yerine versiyon kullanarak Jitpack erişimini garantiliyoruz
+        classpath("com.github.LagradOst:CloudStream-Gradle-Plugin:master-SNAPSHOT")
     }
 }
 
-// Pluginleri güvenli yoldan apply ediyoruz
 apply(plugin = "com.android.library")
 apply(plugin = "kotlin-android")
 apply(plugin = "com.lagradost.cloudstream3")
@@ -20,14 +19,12 @@ apply(plugin = "com.lagradost.cloudstream3")
 configure<CloudstreamExtension> {
     authors = listOf("MoOnCrOwN")
     language = "tr"
-    description = "Mooncrown Deneme Eklentisi"
-    // 0: Belirsiz, 1: Çalışıyor, 2: Bozuk, 3: Beta
+    description = "Mooncrown IPTV ve Film Eklentisi"
     status = 1
     tvTypes = listOf("Movie", "TvSeries")
 }
 
 android {
-    // Gradle 8+ için namespace zorunludur
     namespace = "com.mooncrown.deneme"
     compileSdk = 34
 
@@ -47,11 +44,9 @@ android {
 }
 
 dependencies {
-    // Cloudstream ana kütüphanesi (Derleme için gerekli)
-    val csVersion = "657155668e" 
-    compileOnly("com.github.LagradOst:CloudStream:$csVersion")
+    // Cloudstream bağımlılığı (Versiyonu master-SNAPSHOT yaparak güncel tutuyoruz)
+    compileOnly("com.github.LagradOst:CloudStream:master-SNAPSHOT")
     
-    // Yardımcı kütüphaneler
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("com.lagradost:ducktape:1.0.4")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
