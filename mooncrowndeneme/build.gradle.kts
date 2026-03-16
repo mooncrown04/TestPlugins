53d00dbbcec7b73d0938406ed9f5c0f9604a2f59
mooncrown04 <mooncrown04@gmail.com>
2026-03-16 20:44:33 +0300

    Update build.gradle.kts

--- mooncrowndeneme/build.gradle.kts
+++ mooncrowndeneme/build.gradle.kts
@@ -1,20 +1,15 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-
 buildscript {
     repositories {
         google()
         mavenCentral()
-        // JitPack yerine doğrudan Cloudstream'in resmi deposunu ekliyoruz
-        maven("https://replicate.npmjs.com/") 
         maven("https://jitpack.io")
     }
     dependencies {
-        // Plugin'i farklı bir şekilde tanımlayarak 401 hatasını bypass edelim
-        classpath("com.github.LagradOst:CloudStream-Gradle-Plugin:master-SNAPSHOT")
+        // master-SNAPSHOT yerine sabit bir sürüm veya commit deniyoruz
+        classpath("com.github.LagradOst:CloudStream-Gradle-Plugin:966299f061") 
     }
 }
 
-// Plugin uygulama sırasını değiştirelim
 apply(plugin = "com.android.library")
 apply(plugin = "kotlin-android")
 apply(plugin = "com.lagradost.cloudstream3")
@@ -22,10 +17,9 @@ apply(plugin = "com.lagradost.cloudstream3")
 cloudstream {
     authors     = listOf("MoOnCrOwN")
     language    = "tr"
-    description = "GitHub üzerinden dinamik olarak güncellenen Mooncrown sinema arşivi."
+    description = "Mooncrown deneme eklentisi."
     status      = 1
-    tvTypes     = listOf("Movie", "TvSeries")
-    iconUrl     = "https://raw.githubusercontent.com/mooncrown04/TestPlugins/master/icon.png"
+    tvTypes     = listOf("Movie")
 }
 
 android {
@@ -35,23 +29,13 @@ android {
     defaultConfig {
         minSdk = 21
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
-    // Cloudstream ana kütüphanesini en stabil sürümle bağlayalım
-    val csVersion = "657155668e" // Jitpack yerine güvenli bir commit
+    // Ana Cloudstream kütüphanesi
+    val csVersion = "657155668e" 
     compileOnly("com.github.LagradOst:CloudStream:$csVersion")
     
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("com.lagradost:ducktape:1.0.4")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
 }
