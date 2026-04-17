8ce960b83d4eb2a54dfc8cd2f9cada179fade9c6
mooncrown04 <mooncrown04@gmail.com>
2026-04-17 19:43:52 +0300

    Update MoOnCrOwNAlways.kt

--- MoOnCrOwNAlways/src/main/kotlin/com/mooncrown/MoOnCrOwNAlways.kt
+++ MoOnCrOwNAlways/src/main/kotlin/com/mooncrown/MoOnCrOwNAlways.kt
@@ -34,7 +34,7 @@ import kotlin.math.min
 
 // --- Ana Eklenti Sınıfı ---
 class MoOnCrOwNAlways(private val sharedPref: SharedPreferences?) : MainAPI() {
-    override var mainUrl = "https://dl.dropbox.com/scl/fi/piul7441pe1l41qcgq62y/powerdizi.m3u?rlkey=zwfgmuql18m09a9wqxe3irbbr"
+    override var mainUrl = "https://dl.dropbox.com/scl/fi/r4p9v7g76ikwt8zsyuhyn/sile.m3u?rlkey=esnalbpm4kblxgkvym51gjokm"
     override var name = "MoOnCrOwN always FULL"
     override val hasMainPage = true
     override var lang = "tr"
