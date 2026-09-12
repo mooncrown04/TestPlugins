36971e02361b4fdb0d94535782f1d16b85069372
mooncrown04 <mooncrown04@gmail.com>
2026-09-13 02:22:04 +0300

    Update build.gradle.kts

--- mooncs3/build.gradle.kts
+++ mooncs3/build.gradle.kts
@@ -1,26 +1,13 @@
-version = 3
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 
-dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-}
+// Üst taraftaki plugins ve dependencies bloklarından sonra:
 
-android {
-    buildFeatures {
-        buildConfig = true
-    }
-
-    defaultConfig {
-        val apiKey = project.findProperty("tmdbApiKey")?.toString() ?: ""
-        buildConfigField("String", "TMDB_SECRET_API", "\"$apiKey\"")
-    }
-}
-
-cloudstream {
+configure<CloudstreamExtension> {
     name.set("Galatasaray Font Eklentisi")
     description.set("Uygulamanın genel yazı tipini Galatasaray temasındaki özel font ile değiştirir.")
     authors.set(listOf("Aytac Afsar"))
     
-    status.set(1) // Veya com.lagradost.cloudstream3.gradle.Status.Working
+    status.set(1) // 1 = Working
     tvTypes.set(listOf("Others"))
     iconUrl.set("https://raw.githubusercontent.com/.../icon.png")
 }
