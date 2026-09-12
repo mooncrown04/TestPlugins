3cd63a8020f7f374b4a21010de70c5922152ecd4
mooncrown04 <mooncrown04@gmail.com>
2026-09-13 01:59:35 +0300

    Update cloudstream configuration for Galatasaray Font

--- mooncs3/build.gradle.kts
+++ mooncs3/build.gradle.kts
@@ -16,18 +16,11 @@ android {
 }
 
 cloudstream {
-    authors     = listOf("MoOnCrOwN","GitLatte", "patr0nq", "keyiflerolsun")
-    language    = "tr"
-    description = "powerboard`un sinema arşivi"
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-    **/
-    status  = 1 // will be 3 if unspecified
-    tvTypes = listOf("Movie")
-    iconUrl = "https://raw.githubusercontent.com/GitLatte/Sinetech/master/img/powersinema/powersinema.png"
+    name.set("Galatasaray Font Eklentisi")
+    description.set("Uygulamanın genel yazı tipini Galatasaray temasındaki özel font ile değiştirir.")
+    authors.set(listOf("Aytac Afsar"))
+    
+    status.set(1) // Veya com.lagradost.cloudstream3.gradle.Status.Working
+    tvTypes.set(listOf("Others"))
+    iconUrl.set("https://raw.githubusercontent.com/.../icon.png")
 }
