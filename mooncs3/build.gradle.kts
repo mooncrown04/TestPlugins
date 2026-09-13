235ee1e9e523e961017437798d3aa31fbb78a398
mooncrown04 <mooncrown04@gmail.com>
2026-09-13 03:17:59 +0300

    Update build.gradle.kts

--- mooncs3/build.gradle.kts
+++ mooncs3/build.gradle.kts
@@ -1,15 +1,13 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-
 plugins {
     id("com.lagradost.cloudstream3.gradle")
 }
 
 cloudstream {
-    name.set("Galatasaray Font Eklentisi")
-    description.set("Uygulamanın genel yazı tipini Galatasaray temasındaki özel font ile değiştirir.")
-    authors.set(listOf("Aytac Afsar"))
+    name        = "Galatasaray Font Eklentisi"
+    description = "Uygulamanın genel yazı tipini Galatasaray temasındaki özel font ile değiştirir."
+    authors     = listOf("Aytac Afsar")
     
-    status.set(1) // 1: Working (Çalışıyor)
-    tvTypes.set(listOf("Others"))
-    iconUrl.set("https://raw.githubusercontent.com/.../icon.png")
+    status      = 1
+    tvTypes     = listOf("Others")
+    iconUrl     = "https://raw.githubusercontent.com/.../icon.png"
 }
